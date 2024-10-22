9c0e1d605bd5138fe380fddf8b55fc7e8c85d15d
Dennis Gimbergsson <dennis.gimbergsson@tradera.com>
2024-10-22 16:27:10 +0200

    feat: Created IcaTestApp and MainActivity

---
+++ app/src/main/java/se/dennisgimbergsson/icatest/IcaTestApp.kt
@@ -0,0 +1,7 @@
+package se.dennisgimbergsson.icatest
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class IcaTestApp : Application()
---
+++ app/src/main/java/se/dennisgimbergsson/icatest/ui/screens/MainActivity.kt
@@ -0,0 +1,24 @@
+package se.dennisgimbergsson.icatest.ui.screens
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+        setContent {
+            Box {
+                Text("Hello world")
+            }
+        }
+    }
+}
