17315d78ddbe28aba1bd50640dd1ece1f79c097c
Dennis Gimbergsson <dennis.gimbergsson@tradera.com>
2024-10-22 16:36:16 +0200

    feat: Created ICATestTheme, ReduxViewModel and Constants

---
+++ app/src/main/java/se/dennisgimbergsson/icatest/ui/theme/Theme.kt
@@ -0,0 +1,36 @@
+package se.dennisgimbergsson.icatest.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Color.White,
+    primaryContainer = Color.Black,
+    background = Color.Black,
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Color.Black,
+    primaryContainer = Color.White,
+    background = Color.White,
+)
+
+@Composable
+fun ICATestTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        content = content
+    )
+}
---
+++ app/src/main/java/se/dennisgimbergsson/icatest/utils/Constats.kt
@@ -0,0 +1,9 @@
+package se.dennisgimbergsson.icatest.utils
+
+object Constants {
+
+    object Api {
+        const val BASE_URL = "https://jsonplaceholder.typicode.com"
+        const val TIMEOUT_SECONDS = 15L
+    }
+}
